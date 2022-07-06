15a95ad75471e14efeb4445e053976d1f0a72703
lxy93 <1406062098@qq.com>
2022-07-06 21:26:54 +0800

    项目结构初始化

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+app
+  .use(ElementPlus, { size: "small", zIndex: 3000 })
+  .use(store)
+  .use(router)
+  .mount("#app");
